e87861d4d5848050cc482866e9817a97ba71b5b6
hluckyr <137447730+hluckyr@users.noreply.github.com>
2023-09-14 16:15:52 -0400

    develop

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
-var characterCount
-var specialChat
-var numbers
-var upperCase
-var lowerCase
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
