09a9b94048503f199c4fa454ef52909060d03cf6
hluckyr <137447730+hluckyr@users.noreply.github.com>
2023-09-18 07:48:01 -0400

    variables and functions

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,67 @@
-// Assignment Code
+// Assignment code here
+
+//variables for password
+var charLen = 8;
+var choiceArr = [];
+
+var specialChar = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '/', '[', '{', ']', '}'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numericalChar = "0123456789";
-var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
-
-function generatePassword() {
-  
-//1 promt user
-//a. pw len 8-125
-//b. lwca, upca, num, spec
-//2 validate input
-//3 display pw 
-
-    var passwordLen = prompt("How many characters in your password? (8-128)");
-    passwordLen = parseInt(passwordLen);
-
-    var password = "generatePassword";
-    var passwordChar = document.querySelector("#password");
-    passwordText.value = password;
-
-    if (passwordLen < 8) {
-        alert("Password must have more than 7 characters!");
-        return "";
-    }
-    if (passwordLen > 128) {
-        alert("Password cant have more than 128 characters!");
-        return ""
-    }
-
-    var lowercaseChar = confirm("lowercase characters?");
-    if (lowercaseChar) {
-        passwordChar += lowercaseChar;
-    }
-
-    var uppercaseChar = confirm("uppercase letters?");
-    if (uppercaseChar) {
-        passwordChar += uppercaseChar;
-    }
-
-    var numericalChar = confirm("numbers?");
-    if (numericalChar) {
-        passwordChar += numericalChar;
-    }
-
-    var specialChar = confirm("special characters?");
-    if (specialChar) {
-        passwordChar += specialChar;
-    }
-    for (var i = 0; i < passwordLen; i++) {
-        password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
-    }
-
-    return "generated password";
-}
-
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//start prompts to generate pw
+function getPrompts() {
+    choiceArr = [];
+    charLen = parseInt(prompt("how many characters in password? must be 8-128"));
+
+    if (isNaN(charLen) || charLen < 8 || charLen > 128) {
+        alert("password must be between 8-128 characters");
+        return false;
+    }
+
+    if (confirm("special characters in password?")) {
+        choiceArr = choiceArr.concat(specialChar);
+    }
+
+    if (confirm("lowercase letters in password?")) {
+        choiceArr = choiceArr.concat(lowerCase);
+    }
+
+    if (confirm("uppercase letters in password?")) {
+        choiceArr = choiceArr.concat(upperCaseCase);
+    }
+
+    if (confirm("numbers in password?")) {
+        choiceArr = choiceArr.concat(number);
+    }
+
+    return true;
+}
+
+// Write password to the #password input
+function writePassword() {
+    var correctPromtps = getPrompts();
+    var passwordText = document.querySelector("#password");
+
+    if (correctPromtps) {
+        var newPassword = generatePassword();
+        passwordText.value = newPassword;
+    }
+    else { passwordText.value = ""; }
+}
+
+//
+function generatePassword() {
+    var password = '';
+    for (var i = 0; i < charLen; i++) {
+        var randomLetter = Math.floor(Math.random() * choiceArr.length);
+        password = password + choiceArr[randomLetter];
+    }
+    return password;
+}
